6209c6e575275263cf76a741cc14adc8953ad792
Rocio-Peralta <1rocioperalta@gmail.com>
2023-12-13 17:58:25 +1300

    add api and change index

---
+++ client/apis/api.ts
@@ -0,0 +1,18 @@
+import request from 'superagent'
+
+import { Todo, TodoDraft } from '../../models/todos'
+
+const url = '/api/v1/todos/'
+
+export async function getTodos() {
+  const res = await request.get(url)
+  return res.body as Todo[]
+}
+
+export function addTodos(task: TodoDraft) {
+  return request.post(url).send(task)
+}
+
+export function deleteTodos(id: number) {
+  return request.delete(`${url}${id}`);
+}
