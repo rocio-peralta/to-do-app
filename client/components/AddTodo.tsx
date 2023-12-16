d64abe36dd88141fc526b6e71e5d98b226ce8d62
Rocio-Peralta <1rocioperalta@gmail.com>
2023-12-16 20:19:17 +1300

    styling button delete and update

--- client/components/AddTodo.tsx
+++ client/components/AddTodo.tsx
@@ -30,21 +30,26 @@ function AddTodo({ onEnterPressed }: AddTodoProps) {
 
   return (
     <>
-      <div className="px-4 py-2">
-        <h1 className="text-gray-800 font-bold text-2xl uppercase">T0-DO LIST</h1>
+      <div className="px-4 py-2 flex justify-center">
+        <h1 className=" flex items-center text-gray-800 font-bold text-2xl uppercase">
+          T0-DO LIST
+        </h1>
       </div>
-      <form onSubmit={handleSubmit} className='"w-full max-w-sm mx-auto px-4 py-2'>
-       <div className='flex items-center border-b-2 border-teal-500 py-2'>   
-              <input
-                className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                type="text"
-                id="todo-input"
-                name="todo"
-                placeholder="Add your task"
-                value={todosList.todo}
-                onChange={handleChange}
-                />
-       </div>
+      <form
+        onSubmit={handleSubmit}
+        className='"w-full max-w-sm mx-auto px-4 py-2'
+      >
+        <div className="flex items-center border-b-2 border-teal-500 py-2">
+          <input
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            id="todo-input"
+            name="todo"
+            placeholder="Add your task"
+            value={todosList.todo}
+            onChange={handleChange}
+          />
+        </div>
       </form>
     </>
   )
--- client/components/List.tsx
+++ client/components/List.tsx
@@ -12,27 +12,62 @@ function List() {
 
   const handleDeleteTodo = (id: number) => {
     dispatch(deleteTodoThenFetch(id))
-   
   }
 
   return (
-   
-        <ul className="divide-y divide-gray-200 px-4">
-          {todos.map((todo) => (
-            <li className="py-4" key={todo.id}>
-              <div className="flex items-center">
-                  <input id="todo1" name="todo1" type="checkbox"
-                    className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded"></input>
-                <div className='ml-3 block text-gray-900 text-lg font-medium'>
-                {todo.todo}
-                </div>
-                <button
-                  className="h-4 w-4 text-teal-800 focus:ring-teal-500 border-gray-300 rounded"
-                  onClick={() => handleDeleteTodo(todo.id)}
-                  >x</button>
-              </div>
-            </li>
-          ))}
+    <ul className="divide-y divide-gray-200 px-4">
+      {todos.map((todo) => (
+        <li className="py-4" key={todo.id}>
+          <div className="flex items-center">
+            <input id="todo1" name="todo1" type="checkbox" className=""></input>
+            <div className="ml-3 block text-gray-900 text-xl font-medium">
+              {todo.todo}
+            </div>
+            <button
+              onClick={() => handleDeleteTodo(todo.id)}
+              type="button"
+              className="text-white bg-red-400 hover:bg-red-500  font-medium rounded-lg text-sm mr-2 px-5 py-2.5 text-center inline-flex items-center ml-auto"
+            >
+              <svg
+                className="w-[18px] h-[18px] text-white dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"
+                />
+              </svg>
+            </button>
+
+            <button
+              type="button"
+              className="text-white bg-gray-400 hover:bg-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center "
+            >
+              <svg
+                className="w-[19px] h-[19px] text--800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 21 21"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.3"
+                  d="M7.418 17.861 1 20l2.139-6.418m4.279 4.279 10.7-10.7a3.027 3.027 0 0 0-2.14-5.165c-.802 0-1.571.319-2.139.886l-10.7 10.7m4.279 4.279-4.279-4.279m2.139 2.14 7.844-7.844m-1.426-2.853 4.279 4.279"
+                />
+              </svg>
+            </button>
+          </div>
+        </li>
+      ))}
     </ul>
   )
 }
