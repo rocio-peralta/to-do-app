b74bb2a83c9e285e97419f267256147963b0f327
Rocio-Peralta <1rocioperalta@gmail.com>
2023-12-28 15:25:51 +1300

    add test for button

--- client/components/App.test.tsx
+++ client/components/App.test.tsx
@@ -6,7 +6,6 @@ import { cleanup } from '@testing-library/react'
 // import matchers from '@testing-library/jest-dom/matchers'
 import userEvent from '@testing-library/user-event'
 
-
 import App from './App'
 import { Provider } from 'react-redux'
 import store from '../store'
@@ -15,17 +14,30 @@ afterEach(cleanup)
 // expect.extend(matchers)
 const user = userEvent.setup()
 
-test('App should render a textbox', () => {
-//Arrange
-render((
+test('App should render a placeholder', () => {
+  //Arrange
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  )
+  //Act
+  const component = screen.getByPlaceholderText(/add your task/i)
+  //Assert
+  expect(component.ariaPlaceholder).contains(/add your task/i)
+})
 
-  <Provider store={store}> 
-    <App />
-  </Provider>
-))
-//Act
-const component = screen.getByRole('textbox')
-//Assert
-expect(component).toBeDefined();
+test('App should render a button', async () => {
+  //Arrange
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  )
+  // Act
+  const button = screen.getByRole('button')
+  await user.click(button)
 
+  //Assert
+  expect(button).not.toBeNull()
 })
